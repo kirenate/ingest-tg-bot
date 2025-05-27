b87f9d42d919c78f25cfff179e33410e016966b6
kirenate <ekirenn@gmail.com>
2025-05-27 22:35:06 +0300

    auto: autocommit

--- main.go
+++ main.go
@@ -28,6 +28,7 @@ func main() {
 	upd := bot.GetUpdatesChan(u)
 
 	for update := range upd {
+		log.Info().Interface("upd", upd).Msg("update.received")
 		//helpers.PrintJSON("", update)
 
 		if update.Message == nil && update.CallbackQuery == nil {
